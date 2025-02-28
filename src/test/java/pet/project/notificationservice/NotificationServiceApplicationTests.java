6f1449bfa8a1029c036d0251206c4b09bb220f6d
Edik <davydiantsedik@mail.ru>
2025-02-28 14:25:30 +0300

    initial

---
+++ src/test/java/pet/project/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pet.project.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
